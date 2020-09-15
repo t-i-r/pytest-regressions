8261ce894207c8c6edd89b6d49286cf87493d8a4
t-i-r <peppa987@gmail.com>
2020-09-15 15:44:50 -0400

    Change df test example it can pass

--- tests/test_df_regression.py
+++ tests/test_df_regression.py
@@ -6,7 +6,7 @@ import pandas as pd
 def test_df_diff(df_regression):
 
     df1 = pd.DataFrame({"a": [1, 2], "b": [3, 4.0]})
-    df2 = pd.DataFrame({"a": [1, 2], "b": [3, 5.0]})
+    df2 = pd.DataFrame({"a": [1, 2], "b": [3, 4.0]})
 
     # pick on df1 or df2 
     df_regression.check(df2)
